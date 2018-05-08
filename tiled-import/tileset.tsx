062f542bf78986f0ed17d9e7546e11fb28f934c8
Tim Mendenhall <timmendenhall@gmail.com>
2018-05-08 18:35:23 -0400

    Adding the tiled-import project

---
+++ tiled-import/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="tiles.png" width="561" height="565"/>
+</tileset>
